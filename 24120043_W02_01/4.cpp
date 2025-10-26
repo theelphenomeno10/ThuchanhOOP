25c577dca7623fa1c61b701d7f42c90436817098
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-10-26 12:21:28 +0700

    add: 4

--- 24120043_W02_01/4.cpp
+++ 24120043_W02_01/4.cpp
@@ -7,13 +7,20 @@ class SoNguyenLon
 {
 private:
     char *_snl = nullptr;
+    static SoNguyenLon snlNhat;
 
 public:
     SoNguyenLon();
+    SoNguyenLon(const long long& ll);
     SoNguyenLon(const char *s);
     SoNguyenLon(const int &so, const int &lanLap);
 
     SoNguyenLon Tru(const SoNguyenLon &snl);
+    int SoSanh(const SoNguyenLon& b);
+    SoNguyenLon Cong(const long long &snl);
+
+    static SoNguyenLon HieuVoiHangSo(const int& s, const SoNguyenLon& snl);
+    static SoNguyenLon SNLMax();
 
     void In(ostream &os);
 };
@@ -29,6 +36,10 @@ SoNguyenLon::SoNguyenLon(const char *s)
     int doDai = strlen(s);
     _snl = new char[doDai + 1];
     strcpy_s(_snl, doDai + 1, s);
+
+    if (SoSanh(snlNhat) > 0){
+        snlNhat = *this;
+    }
 }
 
 SoNguyenLon::SoNguyenLon(const int &so, const int &lanLap)
@@ -39,47 +50,142 @@ SoNguyenLon::SoNguyenLon(const int &so, const int &lanLap)
         _snl[i] = '0' + so;
     }
     _snl[lanLap] = '\0';
+
+    if (SoSanh(snlNhat) > 0){
+        snlNhat = *this;
+    }
+}
+
+int SoNguyenLon::SoSanh(const SoNguyenLon &b){
+    if (strlen(_snl) != strlen(b._snl)){
+        if (strlen(_snl) > strlen(b._snl)){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    return strcmp(_snl, b._snl);
 }
 
 SoNguyenLon SoNguyenLon::Tru(const SoNguyenLon &s)
 {
-    char *soBiTru;
-    char *soTru;
+    const char *soBiTru;
+    const char *soTru;
 
-    int doDai1 = 0, doDai2 = 0;
-    bool isNegative = strcmp(s._snl, _snl) == -1;
+    bool isNegative = SoSanh(s) < 0;
     if (isNegative)
     {
-        doDai1 = strlen(s._snl);
-        doDai2 = strlen(_snl);
-
-        soBiTru = new char[doDai1 + 1];
-        soTru = new char[doDai2 + 1];
-
-        strcpy_s(soBiTru, doDai1 + 1, s._snl);
-        strcpy_s(soTru, doDai2 + 1, _snl);
+        SoNguyenLon a("0");
+        return a;
     }
     else
     {
-        doDai1 = strlen(_snl);
-        doDai2 = strlen(s._snl);
-
-        soBiTru = new char[doDai1 + 1];
-        soBiTru = new char[doDai2 + 1];
-
-        strcpy_s(soBiTru, doDai1 + 1, _snl);
-        strcpy_s(soTru, doDai2 + 1, s._snl);
+        soBiTru = _snl;
+        soTru = s._snl;
     }
 
-    int i1 = doDai1, i2 = doDai2;
-    int carry = 0, a, b, k = 0;
-    char *res = new char[doDai1 + 2];
-    while (carry > 0 || i1 > 0 || i2 > 0)
+    int i1 = strlen(soBiTru) - 1, i2 = strlen(soTru) - 1;
+    int muon = 0, a, b, k = 0;
+    char *res = new char[i1 + 2];
+    while (i1 >= 0 || i2 >= 0)
     {
-        a = soBiTru[i1] - '0';
+        a = i1 < 0 ? 0 : soBiTru[i1] - '0';
         b = i2 < 0 ? 0 : soTru[i2] - '0';
+
+        a -= muon;
+        if (a < b){
+            a += 10;
+            muon = 1;
+        } else {
+            muon = 0;
+        }
+
+        res[k++] = (a - b) + '0';
+        i1--;
+        i2--;
     }
+
+    while (k > 1 && res[k - 1] == '0')
+    {
+        k--;
+    }
+
+    res[k] = '\0';
+
+    for (int i = 0; i < k / 2; i++)
+    {
+        swap(res[i], res[k - i - 1]);
+    }
+
+    SoNguyenLon result(res);
+    delete[] res;
+
+    if (SoSanh(snlNhat) > 0){
+        snlNhat = result;
+    }
+
+    return result;
 }
+
+SoNguyenLon SoNguyenLon::Cong(const long long &ll)
+{
+    char* s = new char[31];
+    sprintf(s, "%lld", ll);
+
+    char* res = new char[max(strlen(_snl), strlen(s)) + 1];
+
+    int i1 = strlen(_snl) - 1, i2 = strlen(s) - 1, du = 0, k = 0;
+
+    while (i1 >= 0 || i2 >= 0){
+        int a = i1 < 0 ? 0 : _snl[i1] - '0';
+        int b = i2 < 0 ? 0 : s[i2] - '0';
+
+        res[k++] = (a + b + du) % 10 + '0';
+        du = (a + b + du) / 10;
+
+        i1--;
+        i2--;
+    }
+
+    if (du > 0){
+        res[k++] = '1';
+    }
+
+    res[k] = '\0';
+
+    for (int i = 0; i < k / 2; i++){
+        swap(res[i], res[k - i - 1]);
+    }
+
+    SoNguyenLon result(res);
+    delete[] s;
+    delete[] res;
+
+    if (SoSanh(snlNhat) > 0){
+        snlNhat = result;
+    }
+
+    return result;
+}
+
+SoNguyenLon SoNguyenLon::HieuVoiHangSo(const int& x, const SoNguyenLon& snl)
+{
+    int n = x;
+    char *s = new char [31];
+    sprintf(s, "%d", n);
+
+    SoNguyenLon a(s);
+    delete[] s;
+    return a.Tru(snl);
+}
+
+SoNguyenLon SoNguyenLon::SNLMax(){
+    return snlNhat;
+}
+
+SoNguyenLon snl;
+SoNguyenLon SoNguyenLon::snlNhat = snl;
 
 void SoNguyenLon::In(ostream &os)
 {
@@ -92,6 +198,9 @@ int main()
     SoNguyenLon snl2("1234567");
     SoNguyenLon snl3(3, 14);
     SoNguyenLon snl4(9, 20);
+    SoNguyenLon snl5 = snl3.Tru(snl2);
+    SoNguyenLon snl6 = SoNguyenLon::HieuVoiHangSo(45678910, snl2);
+    SoNguyenLon snl7 = snl4.Tru(snl3).Cong(123456789);
     snl1.In(cout);
     cout << endl;
     snl2.In(cout);
@@ -100,4 +209,17 @@ int main()
     cout << endl;
     snl4.In(cout);
     cout << endl;
+    snl5.In(cout);
+    cout << endl;
+    snl6.In(cout);
+    cout << endl;
+    snl7.In(cout);
+    cout << endl;
+
+    cout << "SoNguyenLon Max:" << endl;
+    SoNguyenLon::SNLMax().In(cout);
+    cout << endl;
+    system("pause");
+
+    return 0;
 }
